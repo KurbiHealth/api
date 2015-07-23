8a1d09396ede9cdf370fc3e2fe819f7385be3012
Matt Eckman <matteckman@gmail.com>
2015-07-23 19:23:46 -0400

    latest

--- routes/v1/query.js
+++ routes/v1/query.js
@@ -210,7 +210,7 @@ module.exports = function(router,connection,passport,validModels,async,joins){
 
                 // DO DATABASE CALL
                 function(sqlString,callback){
-                    options = {sql: sqlString, nestTables: false};
+                    options = {sql: sqlString, nestTables: true};
                     connection.query(options, function(err, rows) {
                         if(err){
                             returnObj = {query: sqlString, dberr: err}
