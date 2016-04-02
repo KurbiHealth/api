10a61208cf9bcf457ae120690ea2b0f2ab09f856
Matt Eckman <matteckman@gmail.com>
2016-04-02 16:07:57 -0400

    latest

--- routes/v1routes.js
+++ routes/v1routes.js
@@ -15,23 +15,23 @@ console.log('---');
 		// put passport.authenticate() here?????????
 
 		// Website you wish to allow to connect
-		res.setHeader('Access-Control-Allow-Origin', '*');
+		//res.setHeader('Access-Control-Allow-Origin', '*');
 
 		// Request methods you wish to allow
-		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+		//res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
 		// Request headers you wish to allow
-		res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-custom-username,x-custom-token');
+		//res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,x-custom-username,x-custom-token');
 
 		// Set to true if you need the website to include cookies in the requests sent
 		// to the API (e.g. in case you use sessions)
-		res.setHeader('Access-Control-Allow-Credentials', true);
+		//res.setHeader('Access-Control-Allow-Credentials', true);
 
-		if (req.method === 'OPTIONS') {
-			res.send(200);
-		}else{
+		//if (req.method === 'OPTIONS') {
+		//	res.send(200);
+		//}else{
 			next();
-		}
+		//}
 	});
 
 	router.param('model',function(req,res,next,model){
